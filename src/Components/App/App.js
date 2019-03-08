5785f3379a51281228a5abb54ae68ebedbc8b9e9
gleen <gleen.lexry@n2nconnect.com>
2019-03-08 11:15:28 +0800

    updates

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
 import TextFields from '../TextFields';
 import AddListItem from '../AddListItem';
 import CheckboxList from '../CheckboxList/';
 import RemoveListItem from '../RemoveListItem';
-import Grid from '@material-ui/core/Grid';
 
 class App extends Component {
   state = {
-    name: ''
+    name: '',
   };
 
   handleChange = e => {
@@ -16,61 +16,60 @@ class App extends Component {
     });
   };
 
-  addItem = (e) => {
+  addItem = e => {
     const { saveTodo, addTodo, todoList } = this.props;
     e.preventDefault();
     const currentItem = this.state.name;
 
-    if(currentItem === "") {
-        alert("Need to add a Task!")
-        //take the todolist object and return an array of obj values. compare with current name.
-    } else if(Object.values(todoList).includes(currentItem)) {
-         alert("you can't have the same task twice!")
+    if (currentItem === '') {
+      alert('Need to add a Task!');
+      //take the todolist object and return an array of obj values. compare with current name.
+    } else if (Object.values(todoList).includes(currentItem)) {
+      alert("you can't have the same task twice!");
     } else {
       addTodo(this.state.name);
       saveTodo(this.state.name);
       this.setState({
-        name: ''
-      })
+        name: '',
+      });
     }
   };
 
-  removeItem = (e) => {
+  removeItem = e => {
     e.preventDefault();
     const { removeTodo, todoList, removeSaveTodo } = this.props;
-    if(todoList.length !== 0) {
+    if (todoList.length !== 0) {
       removeTodo();
       removeSaveTodo();
     } else {
-      alert('nothing to remove!')
+      alert('nothing to remove!');
     }
-  }
-
+  };
 
   render() {
-    //Todo: Add Props types
-    //Todo: Refactor with proper component names
+    //  Todo: Add Props types
+    //  Todo: Refactor with proper component names
     return (
       <div className="App">
         <TextFields
           handleChange={this.handleChange}
           addItem={this.addItem}
           name={this.state.name}
-         />
-         <Grid container >
+        />
+        <Grid container>
           <Grid item xs={2}>
             <AddListItem
               addItem={this.addItem}
               loading={this.props.saveTodoPending}
             />
-            </Grid>
-            <Grid item xs={2}>
-              <RemoveListItem 
-                handleClick={this.removeItem}
-                loading={this.props.removeTodoPending}
-              />     
-            </Grid>       
-         </Grid>
+          </Grid>
+          <Grid item xs={2}>
+            <RemoveListItem
+              handleClick={this.removeItem}
+              loading={this.props.removeTodoPending}
+            />
+          </Grid>
+        </Grid>
         <CheckboxList
           inputItem={this.state.name}
           todoList={this.props.todoList}
